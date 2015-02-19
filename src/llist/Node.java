f15242cacb5edab42743e5e68aac4d207cb50197
Nikita A Zemlevskiy <zemliak@users.noreply.github.com>
2015-02-19 13:45:38 -0500

    Update Node.java
    update node 2/19

--- src/llist/Node.java
+++ src/llist/Node.java
@@ -5,26 +5,14 @@ public final class Node {
 	Node next;
 	Node previous;
 	public Node(Object that){
-		this.it = that;
-		next = null;
-		previous = null;
+		this(that, null, null);
 	}
 	public Node(Object that, Node next){
-		this(that);
-		this.next = next;
+		this(that, next, null);
 	}
 	public Node(Object that, Node next, Node previous){
-		this(that);
+		this.it = that;
 		this.next = next;
 		this.previous = previous;
 	}
-	/*public static void main(String [] args){
-		Integer i1 = new Integer(4);
-		Integer i2 = new Integer(5);
-		Integer i3 = new Integer(6);
-		Node n1 = new Node(i1);
-		Node n2 = new Node(i2);
-		Node n3 = new Node(i3);
-		n1.setNext(n2);
-	}*/
 }
